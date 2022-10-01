668644ad2e09a3b7185bd2f98ecf4c191648e676
iker-gonzalez <ikgonzal@aiagasesores.com>
2022-10-01 10:09:41 +0200

    NMF stack done

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: ikgonzal <ikgonzal@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 08:24:27 by ikgonzal          #+#    #+#             */
-/*   Updated: 2022/09/29 09:24:01 by ikgonzal         ###   ########.fr       */
+/*   Updated: 2022/10/01 10:08:53 by ikgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,38 @@ namespace ft {
 		public:
 				//member types
 				typedef Container																	container_type;
-				typedef typename Container::value_type												value_type;
-				typedef typename Container::size_type												size_type;
-				typedef typename Container::reference												reference;
-				typedef typename Container::const_reference											const_reference;
+				typedef typename container_type::value_type											value_type;
+				typedef typename container_type::size_type											size_type;
+				typedef typename container_type::reference											reference;
+				typedef typename container_type::const_reference									const_reference;
 	
 				//member functions
-				explicit stack (const container_type& ctnr = container_type())						{ _container = cont; };
+				explicit stack (const container_type& ctnr = container_type())						{ _container(container_type); };
 				Stack(Stack const &other)															{ _container = other._container; };
 				~Stack()																			{ };
 				Stack& operator=(const Stack &other)												{ if (this == &other) return *this;
 																									_container = other._container;
-																									return *this; };
-				bool empty() const																	{ return _container.empty(); };
-				size_type size() const																{ return _container.size(); };
+																									return *this; };	
+				//element access
 				value_type& top()																	{ return _container.back(); };
 				const value_type& top() const														{ return _container.back(); };
+
+				//capacity
+				bool empty() const																	{ return _container.empty(); };
+				size_type size() const																{ return _container.size(); };
+				
+				//modifiers
+				void push (const value_type& val)													{ return _container.push_back(val); };
+				void pop()																			{ return _container.pop_back(); };
+
+				//non-member functions
+				friend bool operator==( const Stack &lhs, const Stack &rhs )						{ return lhs._container == rhs._container; };
+				friend bool operator!=( const Stack &lhs, const Stack &rhs )						{ return lhs._container != rhs._container; };
+				friend bool operator<( const Stack &lhs, const Stack &rhs )							{ return lhs._container < rhs._container; };
+				friend bool operator<=( const Stack &lhs, const Stack &rhs )						{ return lhs._container <= rhs._container; };
+				friend bool operator>( const Stack &lhs, const Stack &rhs )							{ return lhs._container > rhs._container; };
+				friend bool operator>=( const Stack &lhs, const Stack &rhs )						{ return lhs._container >= rhs._container; };
 	};
 }
-
-
-
 
 #endif

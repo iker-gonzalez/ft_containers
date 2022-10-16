1f0f39bc0b2bb8d5ce27baafb623cc4c643bd966
iker-gonzalez <ikgonzal@aiagasesores.com>
2022-10-16 12:06:12 +0200

    vector iterator added

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: ikgonzal <ikgonzal@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 10:44:52 by ikgonzal          #+#    #+#             */
-/*   Updated: 2022/10/16 09:57:41 by ikgonzal         ###   ########.fr       */
+/*   Updated: 2022/10/16 12:04:55 by ikgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #define VECTOR_BASE_HPP
 
 #include <memory>
+
+#include "random_access_iterator.hpp"
 
 namespace ft {
 	template <class T, class Alloc = std::allocator <T> > 
@@ -53,7 +55,13 @@ namespace ft {
 				~vector ();
 				vector& operator= (const vector& x);
 
-				// *****TODO:     ITERATORS     ****** //
+				//****************** ******************//
+				//************** ITERATORS ************//
+				//****************** ******************//
+				
+				typedef typename ft::random_access_iterator<value_type>				iterator;
+				typedef typename ft::random_access_iterator<const value_type>		const_iterator;
+				//TODO: reverse_iterator
 				
 				//****************** ******************//
 				//************** CAPACITY *************//
@@ -87,7 +95,7 @@ namespace ft {
 
 
 		private:
-				pointer				_array; // Adress of the array - We are using a pointer to allow a dynamic allocation of the memory during runtime of the program
+				pointer				_ptr; // Adress of the array - We are using a pointer to allow a dynamic allocation of the memory during runtime of the program
 				size_type 			_size; // size of the array
 				size_type 			_capacity; //size for the memory
 				allocator_type		_alloc;
@@ -104,26 +112,26 @@ namespace ft {
 		//********************** **********************//
 
 		//Default constructor. Constructs an empty container with a default-constructed allocator.
-		vector(void): _size(0), _capacity(0), _array(NULL) { };
+		vector(void): _size(0), _capacity(0), _ptr(NULL) { };
 
 		//Constructs an empty container with the given allocator alloc.
-		vector(const allocator_type& alloc)
+		explicit vector(const allocator_type& alloc)
 		{
 			this->_size = 0;
 			this->_capacity = 0;
 			this->_alloc = alloc;
-			this->_array = NULL;
+			this->_ptr = NULL;
 		}
 		
 		//Constructs the container with count copies of elements with value value.
-		vector (size_type n, const value_type& val, const allocator_type& alloc)
+		explicit vector (size_type n, const value_type& val, const allocator_type& alloc)
 		{
 			this->_size = n;
 			this->_capacity = n;
 			this->_alloc = alloc;
-			this->_array = _alloc.allocate(this->_capacity);
+			this->_ptr = _alloc.allocate(this->_capacity);
 			for (size_type i = 0; i < n; i++)
-				_alloc.construct(&_array[i], val);
+				_alloc.construct(&_ptr[i], val);
 		}
 
 		//Copy constructor. Constructs the container with the copy of the contents of x.
@@ -132,9 +140,9 @@ namespace ft {
 			this->_size = x._size;
 			this->_capacity = x._capacity;
 			this->_alloc = x._alloc;
-			this->_array = _alloc.allocate(this->_capacity);
+			this->_ptr = _alloc.allocate(this->_capacity);
 			for(size_t i = 0; i < this->_size; i++)
-				_alloc.construct(&_array[i], x_array[i]);
+				_alloc.construct(&_ptr[i], x_ptr[i]);
 		}
 
 		//TODO: template <class InputIterator>
@@ -143,23 +151,47 @@ namespace ft {
 		~vector(void)
 		{
 			for (size_t i = 0; i < this->_size; i++)
-				this->_alloc.destroy(&this->_array[i]);
-			this->_alloc.deallocate(this->_array, this->_capacity);
+				this->_alloc.destroy(&this->_ptr[i]);
+			this->_alloc.deallocate(this->_ptr, this->_capacity);
 		}
 
 		vector& operator= (const vector& x)
 		{
 			for (size_t i = 0; i < this->_size; i++)
-				this->_alloc.destroy(&this->_array[i]);
+				this->_alloc.destroy(&this->_ptr[i]);
 			this->_size = x._size;
 			this->_capacity = x._capacity;
 			for (size_type i = 0; i < n; i++)
-				this->_array[i] = x.val;
+				this->_ptr[i] = x.val;
 			return (*this);
 		}
 
-		// *****TODO:     ITERATORS     ****** //
+		//****************** ******************//
+		//************** ITERATORS ************//
+		//****************** ******************//
 
+		iterator begin (void)
+		{
+			return iterator(_ptr);
+		}
+
+		iterator begin (void) const
+		{
+			return const_iterator(_ptr);
+		}
+
+		iterator end (void)
+		{
+			return iterator(_ptr + _size);
+		}
+
+		iterator end (void) const
+		{
+			return const_iterator(_ptr + _size);
+		}
+
+		//TODO: reverse_iterator
+		
 		//****************** ******************//
 		//************** CAPACITY *************//
 		//****************** ******************//
@@ -195,13 +227,13 @@ namespace ft {
 				throw std::length_error("Trying to reserve more space than max_size => vector::reserve");
 			if (n > this->size())
 			{
-				pointer new_array = _alloc.allocate(n);
+				pointer new_ptr = _alloc.allocate(n);
 				for (int i = 0; i < this->size(); i++) {
-					this->_alloc.construct(&new_array[i], this->_array[i]);
+					this->_alloc.construct(&new_ptr[i], this->_ptr[i]);
 					this->_alloc.destroy(&this->array[i]);
 				}
-				this->_alloc.deallocate(this->_array, this->_capacity);
-				//TODO: ?? set_array(n) ?? //
+				this->_alloc.deallocate(this->_ptr, this->_capacity);
+				//TODO: ?? set_ptr(n) ?? //
 				this->_capacity= n;
 			}
 		
@@ -213,56 +245,56 @@ namespace ft {
 		
 		reference operator[] (size_type n)
 		{
-			return (&(this->_array[n]));
+			return (&(this->_ptr[n]));
 		}
 		
 		const_reference operator[] (size_type n) const
 		{
-			return (&(this->_array[n]));
+			return (&(this->_ptr[n]));
 		}
 
 		reference at (size_type n)
 		{
 			if (n > this->size())
-				throw std::out_of_range("Elemnt trying to be accessed is out of range\n");
-			return (&(this->_array[n]));
+				throw std::out_of_range("Element trying to be accessed is out of range\n");
+			return (&(this->_ptr[n]));
 		}
 		
 		const_reference at (size_type n) const
 		{
 			if (n > this->size())
-				throw std::out_of_range("Elemnt trying to be accessed is out of range\n");
-			return (&(this->_array[n]));
+				throw std::out_of_range("Element trying to be accessed is out of range\n");
+			return (&(this->_ptr[n]));
 		}
 
 		reference front()
 		{
-			return (&(this->_array[0]));
+			return (&(this->_ptr[0]));
 		}
 		
 		const_reference front() const
 		{
-			return (&(this->_array[0]));
+			return (&(this->_ptr[0]));
 		}
 
 		reference back()
 		{
-			return (&(this->_array[this->size() - 1]));
+			return (&(this->_ptr[this->size() - 1]));
 		}
 
 		const_reference back() const
 		{
-			return (&(this->_array[this->size() - 1]));
+			return (&(this->_ptr[this->size() - 1]));
 		}
 
 		value_type* data()
 		{
-			return (*(this->_array));
+			return (*(this->_ptr));
 		}
 
 		const value_type* data() const
 		{
-			return (*(this->_array));
+			return (*(this->_ptr));
 		}
 	};
 
@@ -282,7 +314,7 @@ namespace ft {
 		if (lhs.size() == rhs.size())
 		{
 			for (int i = 0; i < lhs.size(); i++) {
-				if (lhs._array[i] != rhs._array[i])
+				if (lhs._ptr[i] != rhs._ptr[i])
 					return false;
 			}
 			return true;
@@ -296,7 +328,7 @@ namespace ft {
 		if (lhs.size() == rhs.size())
 		{
 			for (int i = 0; i < lhs.size(); i++) {
-				if (lhs._array[i] != rhs._array[i])
+				if (lhs._ptr[i] != rhs._ptr[i])
 					return true;
 			}
 			return false;
@@ -310,7 +342,7 @@ namespace ft {
 		if (lhs.size() == rhs.size())
 		{
 			for (int i = 0; i < lhs.size(); i++) {
-				if (lhs._array[i] < rhs._array[i])
+				if (lhs._ptr[i] < rhs._ptr[i])
 					return true;
 			}
 			return false;
@@ -326,7 +358,7 @@ namespace ft {
 		if (lhs.size() == rhs.size())
 		{
 			for (int i = 0; i < lhs.size(); i++) {
-				if (lhs._array[i] <= rhs._array[i])
+				if (lhs._ptr[i] <= rhs._ptr[i])
 					return true;
 			}
 			return false;
@@ -342,7 +374,7 @@ namespace ft {
 		if (lhs.size() == rhs.size())
 		{
 			for (int i = 0; i < lhs.size(); i++) {
-				if (lhs._array[i] > rhs._array[i])
+				if (lhs._ptr[i] > rhs._ptr[i])
 					return true;
 			}
 			return false;
@@ -358,7 +390,7 @@ namespace ft {
 		if (lhs.size() == rhs.size())
 		{
 			for (int i = 0; i < lhs.size(); i++) {
-				if (lhs._array[i] >= rhs._array[i])
+				if (lhs._ptr[i] >= rhs._ptr[i])
 					return true;
 			}
 			return false;
@@ -367,13 +399,6 @@ namespace ft {
 			return true;
 		return false;
 	}
-	
-	
-	
-	
-	
-	
-	
 }
 
 
